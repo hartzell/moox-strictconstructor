f5a4e5e3cfff4e5ac4377f9b35b6c26e9d9181c8
George Hartzell <hartzell@alerce.com>
2013-04-13 15:34:46 -0700

    POD improvements

--- lib/MooX/StrictConstructor.pm
+++ lib/MooX/StrictConstructor.pm
@@ -28,6 +28,26 @@ declare, then it dies. This is a great way to catch small typos.
 Most of this package was lifted from L<MooX::InsideOut> and most of the Role
 that implements the strictness was lifed from L<MooseX::StrictConstructor>.
 
+=head2 SUBVERTING STRICTNESS
+
+L<MooseX::StrictConstructor> documents two tricks for subverting strictness and
+avoid having problematic arguments cause an exception: handling them in BUILD
+or handle them in BUILDARGS.
+
+In L<MooX::StrictConstructor> you can use a BUILDARGS function to handle them,
+e.g. this will allow you to pass in a parameter called "spy" without raising an
+exception.  Useful?  Only you can tell.
+
+   sub BUILDARGS {
+       my ($self, %params) = @_;
+       my $spy delete $params{spy};
+       # do something useful with the spy param
+       return \%params;
+   }
+
+Because C<BUILD> methods are run after an object has been constructed and this
+code runs before the object is constructed the C<BUILD> trick will not work.
+
 =cut
 
 use strictures 1;
@@ -47,6 +67,19 @@ sub import {
         'Method::Generate::Constructor::Role::StrictConstructor',
     );
 }
+
+=head1 BUGS
+
+=for :list
+
+* A class that uses L<MooX::StrictConstructor> but extends another class that
+  does not will not be handled properly.  This code hooks into the constructor
+  as it is being strung up (literally) and that happens in the parent class,
+  not the one using strict.
+* L<MooseX::StrictConstructor> documents a trick for subverting strictness
+  using BUILD.  This does not work here because strictness is enforced in the
+  early stage of object construction but the BUILD subs are run after the
+  objects has been built.
 
 =head1 SEE ALSO
 
